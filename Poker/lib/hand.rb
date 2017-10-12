c8f26538057782e78c55ceb8bc7a3006e24e2e81
Jeffrey Ren <jeffyiren@gmail.com>
2017-10-11 17:59:04 -0700

    Add check_hand

--- Poker/lib/hand.rb
+++ Poker/lib/hand.rb
@@ -13,4 +13,25 @@ class Hand
       @hand << @deck.shift
     end
   end
+
+  def check_hand(hand)
+    return :royal_flush if royal_flush?(hand)
+    return :straight_flush if straight_flush?(hand)
+    return :four_of_a_kind if four_of_a_kind?(hand)
+    return :full_house if full_house?(hand)
+    return :flush if flush?(hand)
+    return :straight if straight?(hand)
+    return :three_of_a_kind if three_of_a_kind?(hand)
+    return :two_pair if two_pair?(hand)
+    return :pair if pair?(hand)
+    return :trash if trash?(hand)
+
+  end
+
+  def royal_flush?(hand)
+    
+  end
+
+
+
 end
